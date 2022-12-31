eba61f1e20a0c804c625badaabe607f94e58997a
Roberto Graca <robertograca@ua.pt>
2022-12-31 17:36:23 +0000

    Add QR Code generator

--- app/src/main/java/com/example/oart/MainActivity.kt
+++ app/src/main/java/com/example/oart/MainActivity.kt
@@ -1,32 +1,27 @@
 package com.example.oart
 
-import android.annotation.SuppressLint
-import android.content.Context
 import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.AttributeSet
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
-import android.view.View
-import android.widget.ActionMenuView
-import android.widget.Button
+import android.widget.ImageView
 import androidx.activity.result.ActivityResultLauncher
 import androidx.appcompat.app.AlertDialog
-import androidx.appcompat.view.menu.ActionMenuItemView
 import androidx.fragment.app.Fragment
-import androidx.navigation.ActivityNavigator
-import androidx.navigation.findNavController
 import com.example.oart.databinding.ActivityMainBinding
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.journeyapps.barcodescanner.CaptureActivity
 import com.journeyapps.barcodescanner.ScanContract
 import com.journeyapps.barcodescanner.ScanOptions
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.MultiFormatWriter
+import com.google.zxing.WriterException
+import com.journeyapps.barcodescanner.BarcodeEncoder
 
 class MainActivity : AppCompatActivity() {
 
@@ -53,6 +48,7 @@ class MainActivity : AppCompatActivity() {
             }
             true
         }
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -64,7 +60,7 @@ class MainActivity : AppCompatActivity() {
         when(item.itemId){
             R.id.exit -> { FirebaseAuth.getInstance().signOut();
             startActivity(Intent(this, SignInActivity::class.java))}
-            R.id.read_qrcode -> {}
+            R.id.read_qrcode -> {generateQR()}
             R.id.scan_qrcode -> {
                 scanCode()
             }
@@ -77,6 +73,27 @@ class MainActivity : AppCompatActivity() {
             replace(R.id.fragmentContainerView,fragment)
             commit()
         }
+
+    private fun generateQR() {
+        val text = FirebaseAuth.getInstance().currentUser?.email.toString()
+        val writer = MultiFormatWriter()
+        try {
+            val matrix = writer.encode(text, BarcodeFormat.QR_CODE, 600, 600)
+            val encoder = BarcodeEncoder()
+            val bitmap = encoder.createBitmap(matrix)
+            //set data image to imageview
+            var builder = AlertDialog.Builder(this@MainActivity)
+            builder.setTitle("Your QR Code")
+            var iv: ImageView = ImageView(this)
+            iv.setImageBitmap(bitmap)
+            builder.setView(iv)
+            builder.setCancelable(true)
+            builder.setPositiveButton("OK", DialogInterface.OnClickListener(){ dialogInterface: DialogInterface, i: Int ->
+            }).show()
+        } catch (e: WriterException) {
+            e.printStackTrace()
+        }
+    }
 
     private fun scanCode(){
         var options  = ScanOptions()
@@ -106,9 +123,11 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun addFriend(friend:String){
+        val friendMap = hashMapOf("friend" to friend)
+
         val db = Firebase.firestore
         db.collection(FirebaseAuth.getInstance().currentUser?.email.toString()+"-friends")
-            .add(friend)
+            .add(friendMap)
             .addOnSuccessListener { documentReference ->
                 Log.d("Save", "DocumentSnapshot added with ID: ${documentReference.id}")
             }
